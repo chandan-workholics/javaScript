8e8660879b37aba0d000efb556f7a14059c2dea5
chandan-workholics <141213780+chandan-workholics@users.noreply.github.com>
2024-02-27 08:05:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all js code as newer version
+
+// alert("3 + 3")  // we are using nodejs, not browser
+
+
+let name = "chandan"
+let age = "24"
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "chandan")
+console.log(typeof "age")
+console.log(typeof "null");  //object
+console.log(typeof "undefined")  //undefined
